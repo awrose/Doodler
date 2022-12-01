d3440ad748c19b654d51fec5a5023216b1118b9e
awrose <awrose@udel.edu>
2022-12-01 14:19:35 -0500

    Added DoodleView --> now need to add both the views together and link them so they work simultaneously

--- app/src/main/java/com/example/doodler/MainActivity.java
+++ app/src/main/java/com/example/doodler/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.doodler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        Context context;
+        DoodleView doodleView = new DoodleView(this);
+        setContentView(doodleView);
     }
+
 }

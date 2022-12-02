847e82304cc1faba3f0b88b49a265567e2345929
awrose <awrose@udel.edu>
2022-12-02 01:04:04 -0500

    saving

--- app/src/main/java/com/example/doodler/DoodleView.java
+++ app/src/main/java/com/example/doodler/DoodleView.java
@@ -1,47 +1,69 @@
 package com.example.doodler;
 
+import static com.example.doodler.MainActivity.paint_brush;
+import static com.example.doodler.MainActivity.path;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class DoodleView extends View {
-    public LayoutParams params;
-    public Path path = new Path();
-    public Paint brush = new Paint();
+    /*public LayoutParams params;
+    private Path path = new Path();
+    private Paint brush = new Paint();*/
+    public ViewGroup.LayoutParams params;
+    public static ArrayList<Path> pathList = new ArrayList<>();
+    public static ArrayList<Integer> colorList = new ArrayList<>();
+    public static int current_brush_color = Color.BLACK;
+    public static float current_width = 20f;
+    public static ArrayList<Float> widthList = new ArrayList<>();
+    //public static int current_color = Color.BLACK;
 
     public DoodleView(Context context) {
         super(context);
         init(context);
     }
 
-    public DoodleView(Context context, AttributeSet attrs) {
+    public DoodleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(context);
     }
 
-    public DoodleView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    public DoodleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
         init(context);
     }
 
-    private void init(Context context) {
-        //do stuff that was in your original constructor...
-        brush.setAntiAlias(true);
-        brush.setColor(Color.MAGENTA);
-        brush.setStyle(Paint.Style.STROKE);
-        brush.setStrokeJoin(Paint.Join.ROUND);
-        brush.setStrokeWidth(8f);
-
-        params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+    public DoodleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
     }
+
+    private void init(Context context){
+        paint_brush.setAntiAlias(true);
+        paint_brush.setColor(Color.BLACK);
+        paint_brush.setStyle(Paint.Style.STROKE);
+        paint_brush.setStrokeCap(Paint.Cap.ROUND);
+        paint_brush.setStrokeJoin(Paint.Join.ROUND);
+        paint_brush.setStrokeWidth(20f);
+
+        params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+    }
+
+
+
+
 
 
     @Override
@@ -52,21 +74,31 @@ public class DoodleView extends View {
         switch(event.getAction()){
             case MotionEvent.ACTION_DOWN:
                 path.moveTo(pointX, pointY);
+                invalidate();
                 return true;
             case MotionEvent.ACTION_MOVE:
                 path.lineTo(pointX, pointY);
-                break;
+                pathList.add(path);
+                colorList.add(current_brush_color);
+                widthList.add(current_width);
+                invalidate();
+                return true;
             default:
                 return false;
         }
-        postInvalidate();
-        return false;
+        //postInvalidate();
+        //return false;
     }
 
     @Override
     protected void onDraw(Canvas canvas){
-        canvas.drawPath(path, brush);
+        for(int i =0; i<pathList.size(); i++){
+            paint_brush.setColor(colorList.get(i));
+            paint_brush.setStrokeWidth(widthList.get(i));
+            canvas.drawPath(pathList.get(i), paint_brush);
+            invalidate();
+        }
+        //canvas.drawPath(path, paint_brush);
     }
-
 
 }
--- app/src/main/java/com/example/doodler/MainActivity.java
+++ app/src/main/java/com/example/doodler/MainActivity.java
@@ -2,10 +2,21 @@ package com.example.doodler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import static com.example.doodler.DoodleView.current_brush_color;
+import static com.example.doodler.DoodleView.pathList;
+import static com.example.doodler.DoodleView.colorList;
+import static com.example.doodler.DoodleView.current_width;
+import static com.example.doodler.DoodleView.widthList;
 
 public class MainActivity extends AppCompatActivity {
+    public static Path path = new Path();
+    public static Paint paint_brush = new Paint();
     //Context context;
     //DoodleView doodle = new DoodleView(context);
 
@@ -14,9 +25,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DoodleView doodle = new DoodleView(this);
+
+        Button ColorBtn= (Button) findViewById(R.id.ColorBtn);
+        ColorBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                paint_brush.setColor(Color.MAGENTA);
+                currentColor(paint_brush.getColor());
+            }
+        });
+
+        Button increaseBrushWidth= (Button) findViewById(R.id.increaseBrushSize);
+        increaseBrushWidth.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                paint_brush.setStrokeWidth(200f);
+                currentWidth(paint_brush.getStrokeWidth());
+            }
+        });
+
+        Button clearCanvasBtn= (Button) findViewById(R.id.clearCanvasBtn);
+        clearCanvasBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                widthList.clear();
+                pathList.clear();
+                colorList.clear();
+                path.reset();
+            }
+        });
+
+
+
+
+
+
+    }
+
+    public void currentColor(int c){
+        current_brush_color = c;
+        path = new Path();
+    }
+
+    public void currentWidth(float f){
+        current_width = f;
+        path = new Path();
     }
 
 
 
 
-}
+    }
